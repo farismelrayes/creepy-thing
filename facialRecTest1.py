0cb4c5f04bd222e873493fc46502dcdf6eeead4c
Rilin <tay0008@jmss.vic.edu.au>
2016-03-01 19:29:14 +1100

    Filter now removes photos without faces
    Also changed directory names to be more specific

--- facialRecTest1.py
+++ facialRecTest1.py
@@ -1,19 +1,26 @@
 """
 
-This is the test file, the slow one that has to rerender all the faces at startup.
+Currently testing different things with the saving
+
+TODO:
+-Clean up code (your naming conventions are horrendous)
+-Query all photos with two faces
+-Crop photos to face
+-Etc
 
 """
 
-import os
 import cv2
+import os
 import numpy as np
-import dill
-import pickle
 from PIL import Image
+import json
 
 #Things that make things work
 arguments = ['haarcascade_frontalface_default.xml']
-peopleIDs = []
+IMAGES = []
+LABELS = []
+FACEID = []
 idElement = 0
 
 # Get user supplied values
@@ -22,33 +29,31 @@ faceCascade = cv2.CascadeClassifier(cascPath)
 
 #Creates the face recogniser
 recognizer = cv2.face.createLBPHFaceRecognizer()
-peopleIDs = []
 idElement = 0
 
-
-#Teaches the recognizer based on images in the database
-images = []
-labels = []
-for folder in os.walk('Database'):
+# Learn faces from pictures in database
+for folder in os.walk('database'):
     currentPerson = folder[0].split("\\")[-1]
-    if (currentPerson!='Database'):#Nobody in the school can be named database now or else
-        peopleIDs.append(idElement)#Also hopefully nobody's name is a number
-        peopleIDs.append(currentPerson)
+    if (currentPerson != 'database'):
+        FACEID.append(idElement)
+        FACEID.append(currentPerson)
+        print(folder[2])
         for file in folder[2]:
-            #Check and see if a face is detected in the image
-            #First convert the image to grayscale
-            image_pil = Image.open('Database\\'+currentPerson+'\\'+file).convert('L')
+            image_pil = Image.open('database\\'+currentPerson+'\\'+file).convert('L')
             image = np.array(image_pil, 'uint8')
-            #Detect it
             faces = faceCascade.detectMultiScale(image)
-            for (x, y, w, h) in faces:
-                images.append(image[y: y + h, x: x + w])
-                labels.append(idElement)
-        idElement+=1
-        print('Gathered files on '+currentPerson)
+            if len(faces) == 1:
+                for (x, y, w, h) in faces:
+                    IMAGES.append(image[y: y + h, x: x + w])
+                    LABELS.append(idElement)
+            else:
+                print("Removed file")
+                os.remove('database\\'+currentPerson+'\\'+file)
+        idElement += 1
+        print('Gathered files on: ' + currentPerson)
 
 #Trains the recognizer
-recognizer.train(images,np.array(labels))
+recognizer.train(IMAGES, np.array(LABELS))
 
 #Things that make things work
 arguments = ['haarcascade_frontalface_default.xml']
@@ -58,7 +63,7 @@ cascPath = arguments[0]
 faceCascade = cv2.CascadeClassifier(cascPath)
 
 #Get the video thingy
-video_capture = cv2.VideoCapture(1)
+video_capture = cv2.VideoCapture(0)
 
 # Create the haar cascade
 faceCascade = cv2.CascadeClassifier(cascPath)
@@ -72,10 +77,10 @@ while True:
 
     #Detect faces in the image
     faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
+        gray, 
+        scaleFactor=1.1, 
+        minNeighbors=5, 
+        minSize=(30, 30), 
         #flags=cv2.cv.CV_HAAR_SCALE_IMAGE
     )
 
@@ -84,11 +89,11 @@ while True:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
         #See if there's anyone we recognize in the rectangle
-        recognize_image_pil = Image.fromarray(frame).convert('L')#.crop((x,y,x+w,y+h))#.convert('L')
+        recognize_image_pil = Image.fromarray(frame).convert('L')#.crop((x, y, x+w, y+h))#.convert('L')
         recognize_image = np.array(recognize_image_pil, 'uint8')
 
         person_predicted = recognizer.predict(recognize_image[y: y + h, x: x + w])#, confidence
-        cv2.putText(frame, peopleIDs[peopleIDs.index(person_predicted)+1],(x,y),1,1,1)
+        cv2.putText(frame, FACEID[FACEID.index(person_predicted)+1], (x, y), 1, 1, 1)
 
     # Display the resulting frame
     cv2.imshow('Video', frame)
